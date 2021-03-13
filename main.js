256d244df8f1329f02fe6a452873a0d706b70494
STEVLACH\lachl <lachlan@mensatech.com.au>
2021-03-13 11:33:05 +1000

    renameddataset

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const Apify = require('apify');
 Apify.main(async () => {
     const requestQueue = await Apify.openRequestQueue();
     await requestQueue.addRequest(new Apify.Request({ url: 'https://hipages.com.au/tradesman_names/B' }));
-    const dataset = await Apify.openDataset('hipages');
+    const dataset = await Apify.openDataset('hipagesB');
     // const pseudoUrls = [new Apify.PseudoUrl('https://hipages.com.au/tradesman_names/[.*]')];
     const proxyConfiguration = await Apify.createProxyConfiguration();
     const crawler = new Apify.PuppeteerCrawler({
